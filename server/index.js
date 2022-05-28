26d6c2b1d7967e76f58b17d4ff3b0ed67701accc
rahul0tripathi <rahultripathidev@gmail.com>
2022-05-29 05:27:42 +0530

    vals

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+app.use(cors())
+const port = 5000;
 const { run } = require("../examples/server-runner");
 let locked = false;
 
